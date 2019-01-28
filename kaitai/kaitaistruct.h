d1738323fcff5792d152ff08d391cb50b805243e
yuqiang <932967248@qq.com>
2019-01-28 20:30:50 +0800

    Update kaitaistruct.h

--- kaitai/kaitaistruct.h
+++ kaitai/kaitaistruct.h
@@ -1,16 +1,21 @@
 #ifndef KAITAI_STRUCT_H
 #define KAITAI_STRUCT_H
 
+#include <map>
 #include <kaitai/kaitaistream.h>
+
 
 namespace kaitai {
 
 class kstruct {
 public:
-    kstruct(kstream *_io) { m__io = _io; }
+    kstruct(kstream *_io = 0) : m__io(_io) {}
     virtual ~kstruct() {}
+    virtual kstruct* clone() const { return new kstruct(*this); }
     virtual void _read() {}
     virtual void _write() {}
+    virtual std::string to_string() const { return std::string(); }
+    virtual std::map<std::string, const void*> get_named_data() const { return std::map<std::string, const void*>(); }
 protected:
     kstream *m__io;
 public:
@@ -19,5 +24,6 @@ public:
 };
 
 }
+
 
 #endif
