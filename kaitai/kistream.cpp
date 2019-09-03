47866517a0921bc817d2393e10c3397f723ad013
John Chadwick <johnwchadwick@gmail.com>
2019-09-02 20:58:18 -0700

    Move endianness bits into its own header.

--- kaitai/kistream.cpp
+++ kaitai/kistream.cpp
@@ -1,26 +1,5 @@
 #include <kaitai/kistream.h>
-
-#if defined(__APPLE__)
-#include <machine/endian.h>
-#include <libkern/OSByteOrder.h>
-#define bswap_16(x) OSSwapInt16(x)
-#define bswap_32(x) OSSwapInt32(x)
-#define bswap_64(x) OSSwapInt64(x)
-#define __BYTE_ORDER    BYTE_ORDER
-#define __BIG_ENDIAN    BIG_ENDIAN
-#define __LITTLE_ENDIAN LITTLE_ENDIAN
-#elif defined(_MSC_VER) // !__APPLE__
-#include <stdlib.h>
-#define __LITTLE_ENDIAN     1234
-#define __BIG_ENDIAN        4321
-#define __BYTE_ORDER        __LITTLE_ENDIAN
-#define bswap_16(x) _byteswap_ushort(x)
-#define bswap_32(x) _byteswap_ulong(x)
-#define bswap_64(x) _byteswap_uint64(x)
-#else // !__APPLE__ or !_MSC_VER
-#include <endian.h>
-#include <byteswap.h>
-#endif
+#include <kaitai/endian.h>
 
 #include <iostream>
 #include <vector>
