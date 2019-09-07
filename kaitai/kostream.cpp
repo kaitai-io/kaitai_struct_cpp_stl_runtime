b2f126cebedca300f7021b8370e558b2b0862ba2
John Chadwick <johnwchadwick@gmail.com>
2019-09-07 13:20:16 -0700

    Implement write support for some primitives.
    This should be enough to enable serialization of very basic seqs for the
    C++ code generator in the future.

---
+++ kaitai/kostream.cpp
@@ -0,0 +1,178 @@
+#include <kaitai/kostream.h>
+#include <kaitai/endian.h>
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+kaitai::kostream::kostream(std::ostream* io): kio(io) {
+    m_io = io;
+}
+
+// ========================================================================
+// Integer numbers
+// ========================================================================
+
+// ------------------------------------------------------------------------
+// Signed
+// ------------------------------------------------------------------------
+
+void kaitai::kostream::write_s1(int8_t t) {
+    m_io->put(t);
+}
+
+// ........................................................................
+// Big-endian
+// ........................................................................
+
+void kaitai::kostream::write_s2be(int16_t t) {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    t = bswap_16(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 2);
+}
+
+void kaitai::kostream::write_s4be(int32_t t) {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    t = bswap_32(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 4);
+}
+
+void kaitai::kostream::write_s8be(int64_t t) {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    t = bswap_64(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 8);
+}
+
+// ........................................................................
+// Little-endian
+// ........................................................................
+
+void kaitai::kostream::write_s2le(int16_t t) {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    t = bswap_16(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 2);
+}
+
+void kaitai::kostream::write_s4le(int32_t t) {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    t = bswap_32(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 4);
+}
+
+void kaitai::kostream::write_s8le(int64_t t) {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    t = bswap_64(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 8);
+}
+
+// ------------------------------------------------------------------------
+// Unsigned
+// ------------------------------------------------------------------------
+
+void kaitai::kostream::write_u1(uint8_t t) {
+    m_io->put(t);
+}
+
+// ........................................................................
+// Big-endian
+// ........................................................................
+
+void kaitai::kostream::write_u2be(uint16_t t) {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    t = bswap_16(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 8);
+}
+
+void kaitai::kostream::write_u4be(uint32_t t) {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    t = bswap_32(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 8);
+}
+
+void kaitai::kostream::write_u8be(uint64_t t) {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    t = bswap_64(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 8);
+}
+
+// ........................................................................
+// Little-endian
+// ........................................................................
+
+void kaitai::kostream::write_u2le(uint16_t t) {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    t = bswap_16(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 2);
+}
+
+void kaitai::kostream::write_u4le(uint32_t t) {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    t = bswap_32(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 4);
+}
+
+void kaitai::kostream::write_u8le(uint64_t t) {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    t = bswap_64(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 8);
+}
+
+// ========================================================================
+// Floating point numbers
+// ========================================================================
+
+// ........................................................................
+// Big-endian
+// ........................................................................
+
+void kaitai::kostream::write_f4be(float t) {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    t = bswap_32(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 4);
+}
+
+void kaitai::kostream::write_f8be(double t) {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+    t = bswap_64(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 8);
+}
+
+// ........................................................................
+// Little-endian
+// ........................................................................
+
+void kaitai::kostream::write_f4le(float t) {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    t = bswap_32(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 4);
+}
+
+void kaitai::kostream::write_f8le(double t) {
+#if __BYTE_ORDER == __BIG_ENDIAN
+    t = bswap_64(t);
+#endif
+    m_io->write(reinterpret_cast<char *>(&t), 8);
+}
+
+// ========================================================================
+// Byte arrays
+// ========================================================================
+
+void kaitai::kostream::write_bytes(std::string bytes) {
+    m_io->write(bytes.data(), bytes.length());
+}
