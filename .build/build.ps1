00e7e42bb9dc8e8533dc257f423307e03c5a4e2b
Pablo Rodriguez Avila <pabloro@microsoft.com>
2023-09-29 09:00:00 +0100

    Add BUILD_SHARE_LIBS as option

--- .build/build.ps1
+++ .build/build.ps1
@@ -28,7 +28,7 @@ Push-Location $repoRoot
 $null = New-Item build -ItemType Directory -Force
 cd build
 
-cmake -DCMAKE_PREFIX_PATH="$GTestPath" -DSTRING_ENCODING_TYPE="$EncodingType" -DBUILD_SHARED_LIBS=ON ..
+cmake -DCMAKE_PREFIX_PATH="$GTestPath" -DSTRING_ENCODING_TYPE="$EncodingType" ..
 cmake --build . --config Debug
 cp $GTestPath\debug\bin\*.dll tests\Debug
 cp Debug\kaitai_struct_cpp_stl_runtime.dll tests\Debug
